aacb9cd5e2681dd30d10fa1b7e3e83b835e6460c
KevinFernandez21 <kdfernan@espol.edu.ec>
2024-12-01 23:06:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Navigation from './components/Navigation.js'
+import Show from './components/ShowEstudiante.js'
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Routes>
+        <Route path="/estudiantes" element={<Show/>} />
+        <Route path="/profesores" element={<div>Lista de Profesores</div>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+export const backend = {
+  host: "http://localhost",
+  port: 8080
+}
---
+++ src/components/createEstudiante.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class createEstudiante extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
